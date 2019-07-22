5fc1f2ffe3f0392496dc569b909f072a10699a28
jcottongit <jcotton02@gmail.com>
2019-07-22 10:14:06 +0100

    store storybook

--- stories/app.stories.js
+++ stories/app.stories.js
@@ -7,11 +7,31 @@ import UserResults from '../src/components/UserResults';
 import UserProfile from '../src/components/UserProfile';
 import mockUserList from '../__mocks__/userList';
 import mockUserProfile from '../__mocks__/userProfile';
+import {StateProvider} from '../src/store';
+import {initialState, reducer} from '../src/App';
 
 const {data: {search: {edges: users}}} = mockUserList;
 
 storiesOf('The App', module)
-  .add('Search bar', () => <SearchBar userSearch={() => {}}/>)
+  .add('Search bar', () => {
+    return (
+      <StateProvider initialState={initialState} reducer={reducer}>
+        <SearchBar/>
+      </StateProvider>
+    );
+  })
   .add('Search page', () => <App/>)
-  .add('User results', () => <UserResults users={users}/>)
-  .add('User profile', () => <UserProfile profile={mockUserProfile}/>);
+  .add('User results', () => {
+    return (
+      <StateProvider initialState={{...initialState, userList: users}} reducer={reducer}>
+        <UserResults/>
+      </StateProvider>
+    );
+  })
+  .add('User profile', () => {
+    return (
+      <StateProvider initialState={{...initialState, profile: mockUserProfile}} reducer={reducer}>
+        <UserProfile/>
+      </StateProvider>
+    );
+  });
